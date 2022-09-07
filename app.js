972c6fbb87439e5919251f688499712c459a9674
Luis Rattia <landresrattia@gmail.com>
2022-09-07 14:03:45 -0300

    segunda parte do projeto final da toti, connected to mongodb e app.js

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express ();
+require("dotenv").config();
+const productRoute = require("./routes/product.js");
+
+const app = express();
 const port = process.env.PORT || 3000;
+
+// middlewares
+app.use(express.json());
+app.use("/api", productRoute);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
 app.get("/", (req, res) => {
     res.send("Bem-vindo to my API");
---
+++ routes/product.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const productSchema = require('../models/product.js');
+
+const router = express.Router();
+
+// create product
+
+router.post("/products", (req, res) => {
+    const product = productSchema(req.body);
+    product
+      .save()
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  });
+
+  // get all product
+router.get("/products", (req, res) => {
+    productSchema
+      .find()
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  });
+
+  // get a product
+router.get("/products/:id", (req, res) => {
+    const { id } = req.params;
+    productSchema
+      .findById(id)
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  });
+
+  // delete a product
+router.delete("/products/:id", (req, res) => {
+    const { id } = req.params;
+    productSchema
+      .remove({ _id: id })
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  });
+
+  // update a product 
+router.put("/products/:id", (req, res) => {
+    const { id } = req.params;
+    const { name, age, email } = req.body;
+    productsSchema
+      .updateOne({ _id: id }, { $set: { name, price } })
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  });
+
+  module.exports = router;
