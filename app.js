6f8d7bed81c9bcd28f28cba0dcba111936854687
Luis Rattia <landresrattia@gmail.com>
2022-09-08 17:02:56 -0300

    Connected to Swagger UI

--- app.js
+++ app.js
@@ -2,6 +2,26 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const productRoute = require("./routes/product.js");
+const path = require("path");
+
+//config swagger 
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerSpecs = {
+  definition : {
+    openapi: "3.0.0",
+    info: {
+      title: "node Petshop API",
+      version: "1.0.0"
+    },
+    server: [
+      {
+        url: "http://localhost:3000"
+      }
+    ]
+  },
+  apis: [`${path.join(__dirname, "./routes/*.js" )}`],
+}
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,6 +32,9 @@ app.use("/api", productRoute);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
+app.use('/api-doc', swaggerUI.serve, swaggerUI.setup(swaggerJsdoc(swaggerSpecs)));
+
+// Route
 app.get("/", (req, res) => {
     res.send("Bem-vindo to my API");
   });
