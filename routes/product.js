8b79ac4bd726d0393f9777b5686b7a32606e206a
Luis Rattia <landresrattia@gmail.com>
2022-09-11 21:19:13 -0300

    create to http, swagger UI content

--- routes/product.js
+++ routes/product.js
@@ -3,6 +3,21 @@ const productSchema = require('../models/product.js');
 
 const router = express.Router();
 
+/**
+  * @swagger 
+  *   components:
+  *     schemas:
+  *        products:
+  *          properties:
+  *              name:
+  *                type: string
+  *              price: 
+  *                 type: number
+  *              purchasePrice: 
+  *                 type: number  
+  *    
+  *                 
+  */
 // create product
 
 router.post("/products", (req, res) => {
@@ -20,18 +35,7 @@ router.get("/products", (req, res) => {
       .then((data) => res.json(data))
       .catch((error) => res.json({ message: error }));
   });
-  /**
-  * @swagger
- * /products:
- *  get:
- *    description: Use to request all products
- *    responses:
- *      '200':
- *        description: A successful response
- *
- */
-
-  // get a product
+ 
 router.get("/products/:id", (req, res) => {
     const { id } = req.params;
     productSchema
@@ -59,4 +63,155 @@ router.put("/products/:id", (req, res) => {
       .catch((error) => res.json({ message: error }));
   });
 
-  module.exports = router;
+
+
+
+/**
+* @swagger
+* /api/products:
+*  get:
+*    summary: Get all products
+*    tags: [Product] 
+*    description: Use to request all product
+*    responses:
+*      '200':
+*         content:
+*           aplication/json:
+*            schema:
+*               type: array
+*               items:
+*                 $ref: '#/components/schemas/products'
+*/
+
+
+/**
+* @swagger
+* /api/products:
+*  post:
+*    description: Use to request all product
+*    responses:
+*      '200':
+*        description: A successful response
+* 
+*/
+
+
+
+/**
+* @swagger
+* /api/products:
+*  post:
+*    summary: create a new product
+*    tags: [Product]  
+*    requestBody: 
+*      required: true
+*      content:
+*        aplication/json: 
+*           schema:
+*               $ref: '#/components/schemas/products'
+*      responses:
+*      '200':
+*        description: New product create
+* 
+*/
+
+
+
+/**
+* @swagger
+* /api/products/{Id}:
+*  get:
+*    summary: Return a product
+*    tags: [Product]  
+*    parameters:
+*    -in: path
+*    name: id     
+*    schema:
+*        type: string
+*    responses:
+*      '200':
+*        description: A successful response
+*      content:
+*         aplication/json:
+*          schema:
+*            type: 
+*               items:
+*                 $ref: '#/components/schemas/products'     
+*/
+
+
+
+
+/**
+* @swagger
+* /api/products/{Id}:
+*  get:
+*    summary: Return a product
+*    tags: [Product]  
+*    parameters:
+*    -in: path
+*    name: id     
+*    schema:
+*        type: string
+*    responses:
+*      '200':
+*        description: A successful response
+*      content:
+*         aplication/json:
+*          schema:
+*            type: 
+*               items:
+*                 $ref: '#/components/schemas/products'     
+*/
+
+
+
+
+/**
+* @swagger
+* /api/products/{Id}:
+*  put:
+*    summary: Update a product
+*    tags: [Product]
+*    parameters:
+*    -in: path
+*    name: id     
+*    schema:
+*        type: string
+*    responses:
+*      '200':
+*        description: A successful response
+*      content:
+*         aplication/json:
+*          schema:
+*            type: 
+*               items:
+*                 $ref: '#/components/schemas/products'     
+*/
+
+
+
+
+/**
+* @swagger
+* /api/products/{Id}:
+*  delete:
+*    summary: Delete  a product
+*    tags: [Product]
+*    parameters:
+*     -in: path
+*     name: id    
+*     required: true 
+*    schema:
+*        type: string   
+*    responses:
+*      200:
+*        description: Product delete
+*      400:
+*         description: Product not found
+   
+*/
+
+
+
+module.exports = router;
